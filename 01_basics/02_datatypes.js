600d46b69c883d1f5094f20ed8c3d5c353fc5308
Aditya-Byte <adityapawar4914@gmail.com>
2025-02-08 14:29:53 +0530

    Datatypes in js

---
+++ 01_basics/02_datatypes.js
@@ -0,0 +1,28 @@
+// Types of data in javascript - js
+// As per the standard we are preferring the modern standard of js.
+// Note - You can checkout the ecmascript standard cause you have seen many things like why 1 + "1" = 2 and so on so
+//  mainly the answer of that is that js initially runs on browser and different browsers has set up different standard so that's the reason for that.
+
+
+// Datatypes in js -
+
+// String
+let name = "aditya";
+// Number
+let no = 124
+// Boolean
+let isLoggedIn = true
+// bigint - we have bigint for big integer too
+let f1 = 12.12 // It is also a number.
+// null
+let data1 = null
+// undefined
+let ud;
+// symbol - for uniqueness
+// Note - the above defined datatypes are the primitive datatypes
+
+// If  you want to checkout the type of data - use the typeof keyword
+
+console.table([typeof name, typeof no, typeof isLoggedIn, typeof f1, typeof data1, typeof ud])
+
+// Note - Your code must be readable. -- aaj ka note.
