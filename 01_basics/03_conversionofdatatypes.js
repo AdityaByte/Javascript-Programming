d84a1b025aa0aac8b049abcd9eb1a040afbb0ffe
Aditya-Byte <adityabyte@gmail.com>
2025-02-08 17:35:42 +0530

    Conversion in js Number,String and Boolean conversion

---
+++ 01_basics/03_conversionofdatatypes.js
@@ -0,0 +1,118 @@
+// Why do we need conversion ?
+/* Sometimes the client send some request (taking just an example) and we need to perform some operation to the 
+data values that we are assuming to come out if client send an integer in the form of string then we have to convert that
+out the specified datatype that we want.*/
+// For conversion javascript has some built in function with the datatype names but only the first letter capital.
+// For example : Number(), String(), Boolean() etc.
+
+let score = "32"
+
+console.log(typeof score)
+console.log(typeof(score)) // We can check out the type using the typeof function too.
+
+let numberScore = Number(score)
+
+console.log(typeof numberScore); // Output : number
+
+// if we have something like 
+
+score = "32abc"
+numberScore = Number(score)
+
+console.log(typeof numberScore); // It still gives out the output of Number but the score is not number 
+console.log(numberScore) // It gives NaN -> Not a Number.
+
+score = null
+numberScore = Number(score)
+
+console.log(typeof numberScore); // It still gives out the output of Number but the score is not number 
+console.log(numberScore) // It gives out 0
+
+score = undefined
+numberScore = Number(score)
+
+console.log(typeof numberScore); // It still gives out the output of Number but the score is not number 
+console.log(numberScore) // It gives out NaN -> Not a number.
+
+score = true
+numberScore = Number(score)
+
+console.log(typeof numberScore); // It still gives out the output of Number but the score is not number 
+console.log(numberScore) // It gives out 1
+
+/* 
+Result -> 
+1. Conversion:Number
+"32" => 32
+"32abc" => NaN
+true => 1 and false => 0
+null => 0
+undefined => NaN
+*/
+
+// Now we are converting out to boolean datatype
+
+let isLogged = "true"
+
+let booleanIsLogged = Boolean(isLogged)
+
+console.log(typeof booleanIsLogged)
+console.log(booleanIsLogged) // It gives true.
+
+isLogged = "" // If we take an empty string.
+booleanIsLogged = Boolean(isLogged)
+
+console.log(booleanIsLogged) // The result is false for empty string
+
+isLogged = 1
+booleanIsLogged = Boolean(isLogged)
+ 
+console.log(booleanIsLogged); // Gives True.
+
+isLogged = null
+booleanIsLogged = Boolean(isLogged)
+
+console.log(booleanIsLogged) // Gives false
+
+isLogged = undefined
+booleanIsLogged = Boolean(isLogged)
+
+console.log(booleanIsLogged) // Gives false
+
+/*
+Result -> 
+2. Conversion: to Boolean
+""(empty string) -> false
+"abc"(string) -> true
+1 -> true and 0 -> false
+null -> false and undefined -> false
+*/
+
+// Now we are converting out things to String datatype
+
+let iamdata = 40
+
+let iamstringdata = String(iamdata)
+
+console.log(typeof iamstringdata)
+console.log(iamstringdata)
+
+iamdata = true
+iamstringdata = String(iamdata)
+console.log(iamstringdata) // Gives "true" as true
+
+iamdata = null
+iamstringdata = String(iamdata)
+console.log(iamstringdata); // Gives null
+
+iamdata = undefined
+iamstringdata = String(iamdata)
+console.log(iamstringdata) // gives undefined
+
+
+/*
+Result-> Conversion: to String
+40 -> "40"
+true -> "true" and false -> "false"
+null -> "null" and undefined -> "undefined"
+*/
