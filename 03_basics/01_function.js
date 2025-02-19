5d26302abf9de4ef3a6f9543fd6dc186169cf9d7
Aditya-Byte <adityapawar4914@gmail.com>
2025-02-19 23:26:23 +0530

    Rest operator in js.

--- 03_basics/01_function.js
+++ 03_basics/01_function.js
@@ -23,3 +23,46 @@ function addTwoNumbers(number1, number2) {
 
 const result = addTwoNumbers(4, 1)
 console.log(result);
+
+function showData(data= "i dont know") {
+    return `data is ${data}`
+}
+
+newResult = showData("hello world")
+
+console.log(newResult)
+
+// Varargs in js - If we don't know how many arguments are coming then how can we handle them
+
+function varArgs(...values) { // In this case ... is called rest operator.
+    // ...values store the values in the array.
+    for(let i=0; i<values.length; i++) {
+        console.log(values[i])
+    }
+}
+
+varArgs("aditya", "abhinav", "lakshya", "ashwin")
+
+const user = {
+    username: "aditya",
+    price: 199
+}
+
+function handleObject(anyObject) {
+    console.log(`Username is ${anyObject.username} and price is ${anyObject.price}`);
+}
+
+handleObject(user)
+handleObject({
+    username: "sam",
+    price: 899
+})
+
+const myNewArray = [200, 400, 100, 600]
+
+function returnSecondValue(getArray) {
+    console.log(getArray[1])
+}
+
+returnSecondValue(myNewArray)
+returnSecondValue([1,2,3,4,5])
