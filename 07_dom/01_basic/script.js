db9be255a1a90458b7ebdcbad54b03934c1775a9
Aditya-Byte <adityabyte@gmail.com>
2025-04-09 16:27:02 +0530

    Learned all about DOM

--- 07_dom/01_basic/script.js
+++ 07_dom/01_basic/script.js
@@ -30,9 +30,22 @@ const newElement2 = document.querySelector(".className")
 const newElement3 = document.querySelector("#id")
 
 // If we want to select all the element of that particular id,classname or tag we can do this
-let ndlist = document.querySelectorAll(h2) // It returns a nodelist which is more likely similar to array but not a pure array 
+let ndlist = document.querySelectorAll('h2') // It returns a nodelist which is more likely similar to array but not a pure array 
 // Note : Nodelist don't have functions like map, filter and reduce.
 
 // Here ndlist is the Nodelist so we can access elements like index
 ndlist[0] // First element
 // It has a key of methods like forEach, item and length field too.
+
+// Since there is another way of fetching items using the className
+let list1 = document.getElementsByClassName("list1") // If we have multiple items which are sharing the same classname.
+// Note: here the list1 is a HTMLCollection which is not a pure array but more likely an array.
+// we don't get the forEach and other array specific functions to it.
+
+// So for converting that to array we have a technique that
+const convertedArray = Array.from(list1) // It takes an iterable and a array like object as a parameter.
+
+// Now we can apply forEach and array functions to it.
+convertedArray.forEach((item) => console.log(item))
+
+// document.getElementsByTagName it also returns the HTMLCollection.
